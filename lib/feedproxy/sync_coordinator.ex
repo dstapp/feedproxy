cea2e185b293808948cc5dd000535289b988d784
David Stapp <git@davd.io>
2025-01-29 13:39:50 +0100

    Add basic parsing of rss feeds

--- lib/feedproxy/sync_coordinator.ex
+++ lib/feedproxy/sync_coordinator.ex
@@ -1,12 +1,53 @@
 defmodule Feedproxy.SyncCoordinator do
+  alias Feedproxy.Subscription
+  alias Feedproxy.Repo
+  import SweetXml
+
   def sync_subscriptions do
-    IO.puts("1. fetch all subscriptions")
+    subscriptions = Repo.all(Subscription)
 
-    IO.puts(
-      "2. spawn a process for each of the subscription that fetches the feed, gets items that are newer than last_synced_at, maps and returns them"
-    )
+    subscriptions
+    |> Enum.each(fn subscription ->
+      spawn(__MODULE__, :sync_subscription, [subscription])
+    end)
+  end
 
-    IO.puts("3. wait for all processes to finish and then write new feeditems to db")
-    # @todo is it only about new feed items or also updates?
+  def sync_subscription(%Subscription{} = subscription) do
+    IO.puts("Syncing subscription #{subscription.id}")
+
+    case fetch_feed(subscription.url) do
+      {:ok, feed_content} ->
+        IO.inspect(feed_content)
+
+        items = feed_content
+        |> xpath(~x"//item"l,
+          title: ~x"./title/text()"s,
+          link: ~x"./link/text()"s,
+          description: ~x"./description/text()"s,
+          pub_date: ~x"./pubDate/text()"s
+        )
+
+        IO.inspect(items)
+
+        {:ok, items}
+        # @todo get items that are newer than last_synced_at
+        # @todo write new feeditems to db
+
+      {:error, reason} ->
+        IO.puts("Failed to fetch feed: #{inspect(reason)}")
+    end
+  end
+
+  defp fetch_feed(url) do
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {:ok, body}
+
+      {:ok, %HTTPoison.Response{status_code: status_code}} ->
+        {:error, "HTTP request failed with status code: #{status_code}"}
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        {:error, reason}
+    end
   end
 end
--- mix.exs
+++ mix.exs
@@ -56,7 +56,9 @@ defmodule Feedproxy.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:httpoison, "~> 2.0"},
+      {:sweet_xml, "~> 0.7.3"}
     ]
   end
 
