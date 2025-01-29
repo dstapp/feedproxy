e3a2b5f3aeacbc221f01704241634099639045fa
David Stapp <git@davd.io>
2025-01-29 11:11:31 +0100

    Create feed item schema and migration

---
+++ lib/feedproxy/feed_item.ex
@@ -0,0 +1,25 @@
+defmodule Feedproxy.FeedItem do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "feed_items" do
+    field :title, :string
+    field :url, :string
+    field :published_at, :utc_datetime
+    field :excerpt, :string
+    field :is_read, :boolean, default: false
+    field :is_starred, :boolean, default: false
+    field :subscription_id, :binary_id
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(feed_item, attrs) do
+    feed_item
+    |> cast(attrs, [:title, :published_at, :url, :excerpt, :is_read, :is_starred])
+    |> validate_required([:title, :published_at, :url, :excerpt, :is_read, :is_starred])
+  end
+end
---
+++ priv/repo/migrations/20250129100929_create_feed_items.exs
@@ -0,0 +1,20 @@
+defmodule Feedproxy.Repo.Migrations.CreateFeedItems do
+  use Ecto.Migration
+
+  def change do
+    create table(:feed_items, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :title, :string
+      add :published_at, :utc_datetime
+      add :url, :string
+      add :excerpt, :string
+      add :is_read, :boolean, default: false, null: false
+      add :is_starred, :boolean, default: false, null: false
+      add :subscription_id, references(:subscriptions, on_delete: :nothing, type: :binary_id)
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:feed_items, [:subscription_id])
+  end
+end
